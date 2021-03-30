246369322694e612c13dafc2c8c613a476f73ec3
Addlibs <addlibs@icloud.com>
2021-03-30 22:58:05 +0100

    Added CRUD methods to Model class and hardcoded test values temporarily

--- com/kacperstasiak/repaymentscheduler/ScheduleModel.java
+++ com/kacperstasiak/repaymentscheduler/ScheduleModel.java
@@ -7,7 +7,26 @@ import java.util.ArrayList;
  * @author Kacper Stasiak
  */
 public class ScheduleModel {
+    public void addDebt() {
+        
+    }
+    
+    public void removeDebt() {
+        
+    }
+    
+    public void getDebtDetails() {
+        
+    }
+    
+    public void setDebtDetails() {
+        
+    }
+    
     public ArrayList<ScheduleItem> getSchedule() {
-        return new ArrayList<>();
+        ArrayList<ScheduleItem> list = new ArrayList<>();
+        list.add(new ScheduleItem(new Debt("Payday Loan 1", "ABC1", 1000, 0.275f, 100), 500));
+        list.add(new ScheduleItem(new Debt("Payday Loan 2", "ABC2", 7500, 0.2225f, 2500), 4000));
+        return list;
     }
 }
