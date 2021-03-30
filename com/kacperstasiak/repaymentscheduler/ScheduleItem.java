629702af5a3c7a270eab013ee278475e80844d4b
Addlibs <addlibs@icloud.com>
2021-03-30 22:57:40 +0100

    Added methods to ScheduleItem class

--- com/kacperstasiak/repaymentscheduler/ScheduleItem.java
+++ com/kacperstasiak/repaymentscheduler/ScheduleItem.java
@@ -5,6 +5,23 @@ package com.kacperstasiak.repaymentscheduler;
  * @author Kacper Stasiak
  */
 class ScheduleItem {
-    Debt debtReference; // References which debt to pay
-    int amount;         // Amount recommended to pay, in pence
+    private Debt debt;  // References which debt to pay
+    private int amount; // Amount recommended to pay, in pence
+    
+    public ScheduleItem(Debt debt, int amount) {
+        this.debt = debt;
+        this.amount = amount;
+    }
+    
+    public Debt getDebt() {
+        return debt;
+    }
+    
+    public int getAmount() {
+        return amount;
+    }
+    
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
