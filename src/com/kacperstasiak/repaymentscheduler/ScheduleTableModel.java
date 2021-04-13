8eb9c796e3e508a970920033d21c7cea1960847d
Addlibs <addlibs@icloud.com>
2021-04-13 17:23:54 +0100

    Implemented table model

--- src/com/kacperstasiak/repaymentscheduler/ScheduleTableModel.java
+++ src/com/kacperstasiak/repaymentscheduler/ScheduleTableModel.java
@@ -1,25 +1,25 @@
 package com.kacperstasiak.repaymentscheduler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.table.AbstractTableModel;
 
 /**
  * Encapsulates the Schedule data model for use in Java Swing tables
  * @author Kacper Stasiak
  */
-public class ScheduleTableModel extends AbstractTableModel {
+public final class ScheduleTableModel extends AbstractTableModel {
     private final ArrayList<String[]> data;
     private final String[] columns = {
         "Debt", "Interest rate", "Outstanding balance", "Minimum payment", "Suggested payment"
     };
-    
-    public ScheduleTableModel() {
-        data = new ArrayList<>();
-    }
+    private Model model = null;
 
-    ScheduleTableModel(ScheduleModel model) {
-        // todo
+    ScheduleTableModel(Model model) {
+        this.model = model;
         data = new ArrayList<>();
+        update();
     }
     
     @Override
@@ -41,5 +41,35 @@ public class ScheduleTableModel extends AbstractTableModel {
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         return data.get(rowIndex)[columnIndex];
+    }
+
+    public int getMinimumPaymentSum() {
+        return model.getMinimumPaymentSum();
+    }
+
+    public void setBudgetAmount(double budget) {
+        model.setBudgetAmount(budget);
+    }
+
+    public void update() {
+        Map<Debt, Integer> suggestions = model.getDebtRepaymentSuggestions();
+        
+        data.clear();
+        for (Debt d : model.getDebts()) {
+            String ref = model.getDebtRef(d);
+            double rate = model.getDebtInterestRate(d);
+            int outstanding = model.getDebtOutstandingBalance(d);
+            int minpay = model.getDebtMinimumPayment(d);
+            int suggestPay = suggestions.get(d);
+            
+            String[] row = {
+                ref, 
+                String.format("%.1f%%", rate * 100.0),
+                String.format("£%.2f", outstanding / 100.0),
+                String.format("£%.2f", minpay / 100.0),
+                String.format("£%.2f", suggestPay / 100.0)
+            };
+            data.add(row);
+        }
     }
 }
