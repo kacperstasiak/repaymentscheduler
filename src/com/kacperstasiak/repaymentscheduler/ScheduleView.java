cc45b0871417c867b5f1fbd72dab07be88909472
Addlibs <addlibs@icloud.com>
2021-04-13 12:59:24 +0100

    Refactor Schedule view

--- src/com/kacperstasiak/repaymentscheduler/ScheduleView.java
+++ src/com/kacperstasiak/repaymentscheduler/ScheduleView.java
@@ -1,81 +1,96 @@
 package com.kacperstasiak.repaymentscheduler;
 
-import java.util.ArrayList;
-
 /**
- * The Schedule view, listing debts and amounts to repay for each.
+ * The view part of the MVC structure, displays the debts list and amounts to 
+ * repay for each.
  * @author Kacper Stasiak
  */
 public class ScheduleView {
-
     private ScheduleController controller;
-
-    protected ScheduleView() {
+    private DebtsListFrame mainView;
+    
+    /**
+     * Public constructor for schedule view. A controller must be set before
+     * calling {@link #init() init()} through 
+     * {@link #setController(ScheduleController) setController}
+     */
+    public ScheduleView() {
+        // Begin with a null controller
         this.controller = null;
     }
-
+    
+    /**
+     * Initialises the user interface for use. Throws IllegelStateException
+     * if a controller isn't set up yet
+     */
     public void init() {
+        // Make sure a controller is set up
         if (this.controller == null) {
             throw new IllegalStateException("Controller not set up yet.");
         }
         
-        System.out.println("View initialised.");
+        // Create the GUI
+        java.awt.EventQueue.invokeLater(() -> {
+            ScheduleModel model = controller.getModel();
+            ScheduleTableModel tmodel = new ScheduleTableModel(model);
+            mainView = new DebtsListFrame(tmodel);
+            mainView.setVisible(true);
+        });
     }
-
+    
+    /**
+     * Prepares the user interface for removal
+     */
     public void close() {
-        //throw new UnsupportedOperationException("Not supported yet.");
-        // nothing yet?
+        // Make the GUI invisible and remove all components
+        mainView.setVisible(false);
+        mainView.removeAll();
     }
-
-    public void update() {
-        //throw new UnsupportedOperationException("Not supported yet.");
-        
-        ArrayList<ScheduleItem> schedule = getModel().getSchedule();
-        
-        int num = 1;
-        for (ScheduleItem item : schedule) {
-            float minPay = item.getDebt().getMinimumPayment() / 100.0f;
-            float amount = item.getAmount() / 100.0f;
-            
-            String recommendation = "The minimum payment is " + 
-                    String.format("£%.2f", minPay) + 
-                    " and that's the amount we recommend you pay.";
-            if (minPay != amount) {
-                recommendation = "The minimum payment is " + 
-                        String.format("£%.2f", minPay) + 
-                        " but we recommend making a payment of " + 
-                        String.format("£%.2f", amount);
-            }
-            
-            float rate = item.getDebt().getInterestRate() * 100.0f;
-            
-            System.out.println(
-                    num++ + ". " + item.getDebt().getPayeeName() + 
-                            " (" + item.getDebt().getReference() + ") " + 
-                            " (" + rate + "%): " + recommendation
-            );
-        }
-    }
-
+    
+    /**
+     * Returns the current schedule controller. Throws IllegalStateException if
+     * no controller is set up
+     * @return Schedule controller being used
+     */
     public ScheduleController getController() {
+        // Make sure a controller is set up
         if (this.controller == null) {
             throw new IllegalStateException("Controller not set up yet.");
         }
         return this.controller;
     }
-
+    
+    /**
+     * Returns the data model being used by the controller.
+     * Throws IllegalStateException if controller isn't set up
+     * @return Data model
+     */
     public ScheduleModel getModel() {
-        return getController().getModel();
+        ScheduleController contr = getController(); // throws if not set up
+        return contr.getModel();
     }
-
+    
+    /**
+     * Sets up the controller for this view. Can be called multiple times, 
+     * if a controller is already set up, 
+     * {@link com.kacperstasiak.repaymentscheduler.ScheduleController#shutdown() shutdown()} 
+     * is called on it. Throws IllegelStateException if the passed controller 
+     * is invalid
+     * @param controller The controller instance to use
+     */
     public void setController(ScheduleController controller) {
+        // Make sure the controller is valid
         if (controller == null) {
-            throw new IllegalStateException("Controller not set up yet.");
+            throw new IllegalStateException("Invalid controller.");
         }
+        
+        // If we're already using some controller, shut it down before 
+        // continuing
         if (this.controller != null) {
             controller.shutdown();
         }
+        
+        // Update the controller to the new one
         this.controller = controller;
     }
-    
 }
